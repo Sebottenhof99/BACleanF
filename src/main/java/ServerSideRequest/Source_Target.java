904335794db267bf26de74def01ef705bcdbd7c1
Adam <s7adfish@uni-bonn.de>
2021-01-08 20:44:57 +0100

    Initial commit

---
+++ src/main/java/ServerSideRequest/Source_Target.java
@@ -0,0 +1,22 @@
+package ServerSideRequest;
+
+import org.apache.commons.httpclient.util.HttpURLConnection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URL;
+
+public class Source_Target {
+
+    HttpServletRequest req;
+    HttpServletResponse resp;
+
+    protected void doGet() throws IOException {
+        URL url = new URL(req.getParameter("url"));
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    }
+
+
+
+}
